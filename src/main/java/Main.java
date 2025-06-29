ec5937d0a9cf59d9df1a116e81cdc25b58136b26
Nastya B <2murka2@gmail.com>
2025-06-29 19:30:54 +0300

    add new function from library

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -5,18 +5,16 @@ import geometry.Triangle;
 public class Main {
     public static void main(String[] args) {
         Circle circle = new Circle(10);
-        System.out.println("Circle:");
-        System.out.println("Area: " + circle.getArea());
-        System.out.println("Perimeter: " + circle.getPerimeter());
+        circle.displayInfo();
+
+        System.out.println("------------------------------------------------------");
 
         Rectangle rectangle = new Rectangle(8, 4);
-        System.out.println("\nRectangle:");
-        System.out.println("Area: " + rectangle.getArea());
-        System.out.println("Perimeter: " + rectangle.getPerimeter());
+        rectangle.displayInfo();
 
-        Triangle triangle = new Triangle(3, 4, 7);
-        System.out.println("\nTriangle:");
-        System.out.println("Area: " + triangle.getArea());
-        System.out.println("Perimeter: " + triangle.getPerimeter());
+        System.out.println("------------------------------------------------------");
+
+        Triangle triangle = new Triangle(6, 4, 7);
+        triangle.displayInfo();
     }
 }
