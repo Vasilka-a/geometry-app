51a577d9aa3aeac28c4f67c106e01af23bc014b0
Nastya B <2murka2@gmail.com>
2025-06-29 20:36:10 +0300

    add three-dimensional-shapes library

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,6 +1,7 @@
 import geometry.Circle;
 import geometry.Rectangle;
 import geometry.Triangle;
+import geometry.threeDimensional.Cube;
 import geometry.utils.Converter;
 import geometry.utils.ShapeComparator;
 
@@ -30,5 +31,14 @@ public class Main {
             System.out.println("Rectangle area has larger area");
         } else
             System.out.println("Areas are equal");
+
+        System.out.println("------------------------------------------------------");
+
+        Cube cube = new Cube(12);
+        System.out.println("Cube: ");
+        System.out.println("Volume: " + cube.getVolume());
+        System.out.println("Surface area: " + cube.getSurfaceArea());
     }
+
+
 }
