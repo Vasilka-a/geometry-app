606ef332f08c36da3f4f59946da05cc069a8d46c
Nastya B <2murka2@gmail.com>
2025-06-29 19:59:09 +0300

    add geometry-utils library and converter

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,16 +1,20 @@
 import geometry.Circle;
 import geometry.Rectangle;
 import geometry.Triangle;
+import geometry.utils.Converter;
 
 public class Main {
     public static void main(String[] args) {
-        Circle circle = new Circle(10);
+        Circle circle = new Circle(4);
         circle.displayInfo();
+        double areaCircle = circle.getArea();
+        System.out.println(Converter.metersToCentimeters(areaCircle));// конвертер в см
 
         System.out.println("------------------------------------------------------");
 
-        Rectangle rectangle = new Rectangle(8, 4);
+        Rectangle rectangle = new Rectangle(800, 40);
         rectangle.displayInfo();
+        System.out.println(Converter.centimetersToMeters(rectangle.getPerimeter()));// конвертер в метры
 
         System.out.println("------------------------------------------------------");
 
